65a6536b820a8873efa6679ac041d326ef935563
Pengzhangyan <pengzhengyan@hotmail.com>
2023-12-06 17:03:35 +0800

    feat custom component

--- src/pages/my/my.vue
+++ src/pages/my/my.vue
@@ -1,28 +1,40 @@
 <script setup lang="ts">
 import { useMemberStore } from '@/stores'
+import { http } from '@/utils/http'
 
 const memberStore = useMemberStore()
+const getData = async () => {
+  const res = await http({
+    method: 'GET',
+    url: '/member/profile1',
+  })
+  console.log(res.result)
+}
 </script>
 
 <template>
   <view class="my">
     <view>会员信息：{{ memberStore.profile }}</view>
-    <button
-      @tap="
-        memberStore.setProfile({
-          nickname: '黑马先锋',
-        })
+    <button @tap="
+      memberStore.setProfile({
+        nickname: '黑马先锋',
+        token: '12345',
+      })
       "
-      size="mini"
-      plain
-      type="primary"
-    >
+            size="mini"
+            plain
+            type="primary">
       保存用户信息
     </button>
-    <button @tap="memberStore.clearProfile()" size="mini" plain type="warn">清理用户信息</button>
+    <button @tap="memberStore.clearProfile()"
+            size="mini"
+            plain
+            type="warn">清理用户信息</button>
+    <button size="mini"
+            plain
+            type="warn"
+            @tap="getData">测试请求</button>
   </view>
 </template>
 
-<style lang="scss">
-//
-</style>
+<style lang="scss">//</style>
